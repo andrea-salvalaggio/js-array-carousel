333d8ecda07a3aec538d0cda33ef6e2af7e5f6fa
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-20 15:09:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//Dato un array contenente una lista di sei immagini (url), creare un carosello come nello screenshot allegato.
+
+//Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
